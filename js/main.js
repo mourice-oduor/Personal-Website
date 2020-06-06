7402abdacf7e74340be826264b323405dfb24435
mourice-oduor <otienomaurice364@gmail.com>
2020-06-05 23:19:41 -0400

    Update site

--- js/main.js
+++ js/main.js
@@ -1,17 +1,73 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () { myFunction() };
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// Get the header tag
+var header = document.getElementById("myheader");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = header.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the heaer when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        header.classList.add("sticky")
     } else {
-        navbar.classList.remove("sticky");
+        header.classList.remove("sticky");
     }
 }
+
+// Responsive navbar that hides the ul-lists on the right
+var navList = document.getElementsByClassName("ml-auto");
+function Show() {
+    navList.classList.add("_Menus-show");
+}
+
+function Hide() {
+    navList.classList.remove("_Menus-show");
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) { slideIndex = 1 }
+//     slides[slideIndex - 1].style.display = "block";
+//     setTimeout(showSlides, 4000); // Change image every 2 seconds
+// }
